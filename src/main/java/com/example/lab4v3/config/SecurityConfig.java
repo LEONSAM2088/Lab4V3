9a7fc2854b118dfe2f5d5d58c48de336dd4ecd06
LEONSAM2088 <leonsams@mail.ru>
2022-12-19 01:26:17 +0300

    Cors

--- src/main/java/com/example/lab4v3/config/SecurityConfig.java
+++ src/main/java/com/example/lab4v3/config/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-       http.csrf().disable().cors().and();
+       http.csrf().disable();
        http = http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and();
 
        http = http.exceptionHandling()
